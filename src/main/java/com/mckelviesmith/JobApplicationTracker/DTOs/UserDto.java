3208da76f11c62857cc1589d0e91321fa708bf73
mckelviesmith <smith.m@northeastern.edu>
2023-12-27 17:22:18 -0500

    fix missing NoArgsConstructor

--- src/main/java/com/mckelviesmith/JobApplicationTracker/DTOs/UserDto.java
+++ src/main/java/com/mckelviesmith/JobApplicationTracker/DTOs/UserDto.java
@@ -2,9 +2,11 @@ package com.mckelviesmith.JobApplicationTracker.DTOs;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserDto {
 
   private String name;
